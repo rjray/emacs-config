ce259bfb43dad93c1718844801a44d618e1f9c40
Randy J. Ray <rjray@blackperl.com>
2012-10-12 15:01:19 -0700

    Made the key-shortcuts work under Linux, albeit clumsily.

--- .emacs.d/my-code/setup-ace-jump-mode.el
+++ .emacs.d/my-code/setup-ace-jump-mode.el
@@ -2,9 +2,9 @@
 
 (require 'ace-jump-mode)
 
-(defun add-hyper-char-to-ace-jump-word-mode (c)
+(defun add-hyper-char-to-ace-jump-word-mode (c prefix)
   (define-key global-map
-    (read-kbd-macro (concat "H-" (string c)))
+    (read-kbd-macro (concat prefix (string c)))
     `(lambda ()
        (interactive)
        (setq ace-jump-query-char ,c)
@@ -12,8 +12,14 @@
        (ace-jump-do (concat "\\b"
                             (regexp-quote (make-string 1 ,c)))))))
 
-(loop for c from ?0 to ?9 do (add-hyper-char-to-ace-jump-word-mode c))
-(loop for c from ?A to ?Z do (add-hyper-char-to-ace-jump-word-mode c))
-(loop for c from ?a to ?z do (add-hyper-char-to-ace-jump-word-mode c))
+(let ((prefix (cond (*is-mac* "H-")
+                    (t "C-s-"))))
+  (progn
+    (loop for c from ?0 to ?9 do
+          (add-hyper-char-to-ace-jump-word-mode c prefix))
+    (loop for c from ?A to ?Z do
+          (add-hyper-char-to-ace-jump-word-mode c prefix))
+    (loop for c from ?a to ?z do
+          (add-hyper-char-to-ace-jump-word-mode c prefix))))
 
 (provide 'setup-ace-jump-mode)
