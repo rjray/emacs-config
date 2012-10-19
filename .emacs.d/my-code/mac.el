f71b336d8d5aabab1166e362057cbaa960178e53
Randy J. Ray <rjray@blackperl.com>
2012-10-18 23:18:56 -0700

    Added keybindings for Command+Arrow Keys.

--- .emacs.d/my-code/mac.el
+++ .emacs.d/my-code/mac.el
@@ -13,6 +13,12 @@
 ;; keybinding to toggle full screen mode
 (global-set-key (quote [M-f10]) (quote ns-toggle-fullscreen))
 
+;; Do something with command+arrow keys
+(global-set-key [(super up)] 'beginning-of-buffer)
+(global-set-key [(super down)] 'end-of-buffer)
+(global-set-key [(super left)] 'previous-buffer)
+(global-set-key [(super right)] 'next-buffer)
+
 ;; Ignore .DS_Store files with ido mode
 (add-to-list 'ido-ignore-files "\\.DS_Store")
 
