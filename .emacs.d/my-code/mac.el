a4bc588b80a22aa770734d1f87bfedd4c4ae1621
Randy J. Ray <rjray@blackperl.com>
2017-07-22 16:08:14 -0700

    Remove some unused settings.

--- .emacs.d/my-code/mac.el
+++ .emacs.d/my-code/mac.el
@@ -25,14 +25,8 @@
 (global-set-key [(super left)] 'previous-buffer)
 (global-set-key [(super right)] 'next-buffer)
 
-;; Ignore .DS_Store files with ido mode
-(add-to-list 'ido-ignore-files "\\.DS_Store")
-
 ;; Don't open files from the workspace in a new frame
 (setq ns-pop-up-frames nil)
-
-;; Use aspell for spell checking: brew install aspell --lang=en
-(setq ispell-program-name "/usr/local/bin/aspell")
 
 (provide 'mac)
 ;;; mac.el ends here
