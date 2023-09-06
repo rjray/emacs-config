f71de6ca595682a745df9ee42abebb1961699289
Randy J. Ray <rjray@blackperl.com>
2023-09-06 15:48:41 -0700

    Better noise-suppression on probes

--- setup_deps.sh
+++ setup_deps.sh
@@ -4,13 +4,13 @@
 # configuration.
 
 echo -n "Checking for npm... "
-if which npm > /dev/null; then
+if which npm 2> /dev/null 1> /dev/null; then
     echo "found."
     for pkg in bash-language-server vscode-langservers-extracted \
                                     dockerfile-language-server-nodejs \
                                     typescript-language-server typescript; do
         echo -n "  Checking for $pkg... "
-        if npm -g ls $pkg > /dev/null; then
+        if npm -g ls $pkg 2> /dev/null 1> /dev/null; then
             echo "found."
         else
             echo "not found. Installing $pkg."
@@ -22,12 +22,12 @@ else
 fi
 
 echo -n "Checking for Homebrew... "
-if which brew > /dev/null; then
+if which brew 2> /dev/null 1> /dev/null; then
     echo "found."
     for pkg in fd rg clojure-lsp/brew/clojure-lsp-native openjdk jdtls \
                   marksman texlab yaml-language-server go gopls llvm; do
         echo -n "  Checking for $pkg... "
-        if brew list $pkg > /dev/null; then
+        if brew list $pkg 2> /dev/null 1> /dev/null; then
             echo "found."
         else
             echo "not found. Installing $pkg."
@@ -39,10 +39,10 @@ else
 fi
 
 echo -n "Checking for Python3's pip3... "
-if which pip3 > /dev/null; then
+if which pip3 2> /dev/null 1> /dev/null; then
     echo "found."
     echo -n "  Checking for python-lsp-server... "
-    if pip3 show python-lsp-server > /dev/null; then
+    if pip3 show python-lsp-server 2> /dev/null 1> /dev/null; then
         echo "found."
     else
         echo "not found. Installing python-lsp-server."
@@ -50,7 +50,7 @@ if which pip3 > /dev/null; then
     fi
     for pkg in pylsp-mypy pylsp-rope python-lsp-ruff python-lsp-black; do
         echo -n "  Checking for $pkg... "
-        if pip3 show $pkg > /dev/null; then
+        if pip3 show $pkg 2> /dev/null 1> /dev/null; then
             echo "found."
         else
             echo "not found. Installing $pkg."
@@ -62,11 +62,11 @@ else
 fi
 
 echo -n "Checking for Perl::LanguageServer... "
-if perl -MPerl::LanguageServer -e 0 > /dev/null; then
+if perl -MPerl::LanguageServer -e 0 2> /dev/null 1> /dev/null; then
     echo "found."
 else
     echo "not found. Checking for cpan..."
-    if which cpan > /dev/null; then
+    if which cpan 2> /dev/null 1> /dev/null; then
         echo "found. Installing Perl::LanguageServer..."
         cpan Perl::LanguageServer
     else
@@ -75,11 +75,11 @@ else
 fi
 
 echo -n "Checking for rust-analyzer... "
-if which rust-analyzer > /dev/null; then
+if which rust-analyzer 2> /dev/null 1> /dev/null; then
     echo "found."
 else
     echo -n "not found. Checking for rustup... "
-    if which rustup > /dev/null; then
+    if which rustup 2> /dev/null 1> /dev/null; then
         echo "found. Installing."
         rustup component add rust-analyzer
     else
