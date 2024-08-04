60c7bfea39a9f01d3aaf234586fbae23aa502b4a
Randy J. Ray <rjray@blackperl.com>
2024-08-04 13:16:31 -0700

    Placement code for NV MBP

---
+++ emacs.d/rjray-mlt/placement.el
@@ -0,0 +1,17 @@
+;;; placement.el --- Force size and placement of initial frame
+
+;;; Commentary:
+
+;;; Code:
+
+(setq initial-frame-alist
+      '((top . 22)
+        (left - 0)
+        (width . 180)
+        (height . 70)))
+
+(add-to-list 'default-frame-alist
+             '(font . "Cascadia Code-12"))
+
+(provide 'placement)
+;;; placement.el ends here
