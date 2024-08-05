cb2bcb8f31f26de1df8268e3ad31c33339ee1db3
Randy J. Ray <rjray@blackperl.com>
2024-08-05 16:52:27 -0700

    Config for `view' package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -123,6 +123,47 @@
            (kill-region (region-beginning) (region-end)))
           (t (backward-kill-word arg))))
 
+  (defun my/view-exit ()
+    "Advice function to disable highlighting upon exiting view-mode."
+    (hl-line-mode -1))
+
+  ;; Configuration for `view-buffer' and `view-file'. Mostly taken from Charles
+  ;; Choi.
+  ;; http://yummymelon.com/devnull/enhancing-navigation-in-emacs-view-mode.html
+  (use-package view
+    :config
+    (add-hook
+     'view-mode-hook
+     (lambda ()
+       (cond ((derived-mode-p 'org-mode)
+              (define-key view-mode-map (kbd "p") 'org-previous-visible-heading)
+              (define-key view-mode-map (kbd "n") 'org-next-visible-heading))
+             ((derived-mode-p 'markdown-mode)
+              (define-key view-mode-map (kbd "p") 'markdown-outline-previous)
+              (define-key view-mode-map (kbd "n") 'markdown-outline-next))
+             ((derived-mode-p 'html-mode)
+              (define-key view-mode-map (kbd "p") 'sgml-skip-tag-backward)
+              (define-key view-mode-map (kbd "n") 'sgml-skip-tag-forward))
+             ((derived-mode-p 'python-mode)
+              (define-key view-mode-map (kbd "p") 'python-nav-backward-block)
+              (define-key view-mode-map (kbd "n") 'python-nav-forward-block))
+             ((derived-mode-p 'emacs-lisp-mode)
+              (define-key view-mode-map (kbd "p") 'backward-sexp)
+              (define-key view-mode-map (kbd "n") 'forward-sexp))
+             ((derived-mode-p 'makefile-mode)
+              (define-key view-mode-map (kbd "p") 'makefile-previous-dependency)
+              (define-key view-mode-map (kbd "n") 'makefile-next-dependency))
+             ((derived-mode-p 'c-mode)
+              (define-key view-mode-map (kbd "p") 'c-beginning-of-defun)
+              (define-key view-mode-map (kbd "n") 'c-end-of-defun))
+             (t
+              (define-key view-mode-map (kbd "p") 'scroll-down-command)
+              (define-key view-mode-map (kbd "n") 'scroll-up-command)))))
+
+    (add-hook 'view-mode-hook 'hl-line-mode)
+
+    (advice-add 'View-exit :after #'my/view-exit))
+
   ;; Toggle two most recent buffers
   (fset 'quick-switch-buffer [?\C-x ?b return])
 
@@ -351,6 +392,8 @@
 ;; Corfu (in-buffer completion popup), taken from Protesilaos Stavrou.
 (use-package corfu
   :ensure t
+  :defines corfu-popupinfo-delay
+  :functions (corfu-popupinfo-mode corfu-history-mode)
   :hook (after-init . global-corfu-mode)
   ;; I also have (setq tab-always-indent 'complete) for TAB to complete
   ;; when it does not need to perform an indentation change.
